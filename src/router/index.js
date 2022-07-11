b9ae9ddfa9e00e113cab257016ec83c3463c9d46
Emeka Chukwudozie <emeka.chukwudozie@betpawa.com>
2022-07-11 15:15:40 +0100

    initial front end design

---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/HomeView.vue'
+import AddCategory from "../views/Category/AddCategory";
+import Category from "../views/Category/AllCategory";
+import EditCategory from "../views/Category/EditCategory";
+import AddProduct from "../views/Product/AddProduct";
+import Product from "../views/Product/Product";
+import EditProduct from "../views/Product/EditProduct";
+import ShowDetails from "../views/Product/ShowDetails";
+
+
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/admin/category/add",
+    name: "AddCategory",
+    component: AddCategory,
+  },
+  {
+    path: "/admin/category",
+    name: "AdminCategory",
+    component: Category,
+  },
+  {
+    path: "/admin/category/:id",
+    name: "EditCategory",
+    component: EditCategory
+  },
+  {
+    path: "/admin/product/add",
+    name: "AddProduct",
+    component: AddProduct,
+  },
+  {
+    path: "/admin/product",
+    name: "AdminProduct",
+    component: Product,
+  },
+  {
+    path: "/admin/product/:id",
+    name: "EditProduct",
+    component: EditProduct,
+  },
+  {
+    path : '/product/show/:id',
+    name : 'ShowDetails',
+    component: ShowDetails
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
